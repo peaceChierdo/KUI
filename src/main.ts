6f11cb789b57c39d50fd2064c4e522b2e09f66b8
ah <449780013@qq.com>
2021-06-25 00:19:59 +0800

    整理字体相关文件

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue'
 import './index.css'
 import { router } from './router'
 import 'github-markdown-css'
+import './assets/font/font.css'
 
 // createApp(App).mount('#app')
 const app = createApp(App)
